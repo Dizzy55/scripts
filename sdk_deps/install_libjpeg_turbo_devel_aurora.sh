8e9a6724c1b3b211466e9a84f8a1f89d6f74f310
Dmitrii Remennikov <rem-works@yandex.ru>
2023-09-17 14:54:56 +0300

    SDK deps for Aurora OS

---
+++ sdk_deps/install_libjpeg_turbo_devel_aurora.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ../common_aurora.sh
+
+function sdk_cmd {
+echo "$@" | ssh \
+    -q \
+    -p $SDK_SSH_PORT \
+    -i "$SDK_SSH_ID" \
+    -o "StrictHostKeyChecking=no" \
+    "$SDK_SSH_USER@$SDK_SSH_HOST"
+}
+
+sdk_cmd sdk-assistant list | grep -o 'AuroraOS-.*-base-[^.]*$' | while read TARGET; do
+    PACKAGE=libjpeg-turbo-devel
+
+    echo ""
+    echo "**** Installing ${PACKAGE} on ${TARGET} ****"
+    sdk_cmd sb2 -t ${TARGET} -m sdk-install -R zypper --non-interactive install ${PACKAGE}
+done
