0a92f5e9fb95fdbf527bfef368fc31df3ab53896
Dmitrii Remennikov <rem-works@yandex.ru>
2024-10-12 20:52:50 +0300

    Update qofono-qt5 and qofonoext build deps

--- common_sailfish.sh
+++ common_sailfish.sh
@@ -8,8 +8,6 @@ export SDK_SSH_ROOT=root
 export SDK_SSH_ID="$SDK_ROOT/vmshare/ssh/private_keys/sdk"
 export SDK_VM_NAME='Sailfish SDK Build Engine'
 export SDK_TARGET_PATTERN='SailfishOS-.*-[^.]*$'
-# FIXME: delete
-export SDK_PACKAGE_VENDOR=jolla
 export SDK_ZYPPER_CACHE_DIR=/home/.zypp-cache/packages/jolla/oss
 export EMULATOR_SSH_HOST=localhost
 export EMULATOR_SSH_PORT=2223
